a33cc8d23f7b539675dd053e703c5a9ba1b297b8
Jaklin Jeleva <jaklinj@abv.bg>
2024-03-20 15:01:46 +0200

    added home page component in main module and routing, deleted main module and created only home component

--- mom-knows-best/src/app/app.module.ts
+++ mom-knows-best/src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './header/header.module';
-import { MainModule } from './main/main.module';
+import { HeaderModule } from './core/core.module';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-
+    AboutComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HeaderModule,
-    MainModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
